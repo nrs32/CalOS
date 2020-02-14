a515b7c60450ff9c802475e9e693de0a14057c92
nrs32 <38587739+nrs32@users.noreply.github.com>
2020-02-14 15:05:15 -0500

    add a few more specific error checks

--- FileCopy/copy.c
+++ FileCopy/copy.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <errno.h>
 #include <unistd.h>   // For access
 #include <sys/stat.h> // For stat
 /**
@@ -10,6 +11,8 @@
  * Sources: I used the following C library for finding functions and C syntax (unless otherwise stated)
  * https://www.tutorialspoint.com/c_standard_library/
  * */
+extern int errno;
+
 int main(int argCount, char **args)
 {
     char *srcFile, *destFile;
@@ -31,13 +34,16 @@ int main(int argCount, char **args)
     stat(srcFile, &path_stat);
 
     if (access(srcFile, F_OK|R_OK) == -1 || !S_ISREG(path_stat.st_mode)) { 
-        fprintf(stderr, "\nThe provided source file could not be found or is invalid.\nBe sure to include the file extension and use a regular file.\n");
+        perror("\nThe provided source file could not be found or is invalid.\nBe sure to include the file extension and use a regular file.\n\n");
         return -1;
     }
 
-    // Make sure destFile does not exist
-    if (access(destFile, F_OK) == 0) { 
-        fprintf(stderr, "\nThe provided destination file already exists.\nPlease provide a new file name.\n");
+    // Make sure destFile does not exist (causing errno 2)
+    int destAccessible = access(destFile, F_OK); // <-- call before if-statement to set errno
+    if (!(errno == 2 && destAccessible == -1)) { 
+        errno == 36 
+            ? fprintf(stderr, "\nThe provided destination file name is too long.\n")
+            : fprintf(stderr, "\nThe provided destination file already exists.\nPlease provide a new file name.\n");
         return -1;
     }
 
@@ -48,9 +54,16 @@ int main(int argCount, char **args)
     destFileStream = fopen(destFile, "w");
 
     char fileContent;
+    char putResult;
     // Loop over srcFile and copy contents over to destFile
     while((fileContent = fgetc(srcFileStream)) != EOF) {
-        fputc(fileContent, destFileStream);
+        putResult = fputc(fileContent, destFileStream);
+
+        // If fputc fails it will return EOF
+        if (putResult == EOF) {
+            perror("\nThe contents of the source file could not be written to the destination file");
+            return -1;
+        }
     }
 
     fclose(srcFileStream);
