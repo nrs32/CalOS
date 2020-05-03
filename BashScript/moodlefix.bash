2dc647431f2fbb4a9eec6934eba44e737ac6d30d
nrs32 <nrs32@gold21.cs.calvin.edu>
2020-05-02 22:27:51 -0400

    handle edge cases

--- BashScript/moodlefix.bash
+++ BashScript/moodlefix.bash
@@ -5,7 +5,8 @@ isVerbose=false
 inputFile=""
 
 # Determine if user added verbose argument.
-if [[ "$1" = "-v" || "$1" = "--verbose" ]] ; then
+if [[ "$1" = "-v" || "$1" = "--verbose" ]] ; 
+then
     isVerbose=true
     inputFile="${@:2}" # All but first arg
 else 
@@ -17,17 +18,64 @@ removeSpaces() {
 }
 
 removeDirTrailingCruft() {
-    echo "$1" | sed -e "s/-[0-9]*.zip//g"
+    # Take only the last match for -digits with optional .zip
+    echo "$1" | sed -e "s/-[0-9]*\(\.zip\)\?$//g"
 }
 
 removeSubdirTrailingCruft() {
     echo "$1" | sed -e "s/_[0-9]*_.*//g"
 }
 
-# Create directory for results
+isZip() {
+    if [[ ($(file --mime-type -b "$1") == application/zip 
+        || $(file --mime-type -b "$1.zip") == application/zip 
+        || $(file --mime-type -b "$1.ZIP") == application/zip)
+        && ! "$1" = *docx ]] ; # docx comes up as type application/zip
+    then
+        echo true
+    else 
+        echo false
+    fi
+}
+
+# Make sure inputFile exists
+if [[ !(-f "$inputFile") ]] ;
+then
+    if [[ -f "$inputFile.zip" ]] ;
+    then
+        inputFile="$inputFile.zip"
+    elif [[ -f "$inputFile.ZIP" ]] ;
+    then
+        inputFile="$inputFile.ZIP"
+    else
+        echo "$inputFile can not be found. Please enter an existing zip name."
+        exit
+    fi
+fi
+
+# Make sure file is a zip
+if [[ ! ($(isZip "$inputFile") = true) ]] ; 
+then
+    echo "File must be a valid zip file. $inputFile is not a zip. Please enter an existing zip name."
+    exit
+fi
+
+# Create directory name for results
 noSpaces=$(removeSpaces "$inputFile")
 resultDir=$(removeDirTrailingCruft "$noSpaces")
 
+# Make sure resultDir does not already exist. 
+# Increment name as needed.
+i=1
+newResultDir=$resultDir
+while [[ -d "$newResultDir" ]]
+do
+    newResultDir="$resultDir($i)"
+    i=$((i+1))
+done
+resultDir="$newResultDir"
+
+# Create the directory
 [[ $isVerbose = true ]] && echo "Creating directory for results called $resultDir" && echo
 mkdir "$resultDir"
 
@@ -49,7 +97,8 @@ do
     newDirName=$(removeSubdirTrailingCruft "$noSpacesDir")
     
     # If newDirName already exists
-    if [[ -d "$newDirName" ]] ; then
+    if [[ -d "$newDirName" ]] ; 
+    then
         # Move files in dir to newDirName
         [[ $isVerbose = true ]] && echo "$newDirName already exists. Moving files in $dir to $newDirName" && echo
         files="$dir/*"
@@ -72,12 +121,14 @@ do
     do
         newFileName=$(removeSpaces "$file")
         # If newFileName is different than original
-        if [[ ! -f "$newFileName" ]] ; then
+        if [[ ! -f "$newFileName" ]] ; 
+        then
             mv "$file" "$newFileName"
         fi
         
         # If file is a zip, unzip it and remove zip
-        if [[ $newFileName = *.zip || $newFileName = *.ZIP ]] ; then
+        if [[ $(isZip "$newFileName") = true ]] ; 
+        then
             [[ $isVerbose = true ]] && echo "Unzipping $newFileName" && echo
             unzip -q "$newFileName" -d "$newDirName" # -q suppresses unzip output
             rm "$newFileName"
